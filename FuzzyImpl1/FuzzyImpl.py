5fe2f19d9781f70e0dcb04aa030804fcdfb12824
allan_noot <allanpatrick18@gmail.com>
2017-08-24 23:30:43 -0300

    calc suport

--- FuzzyImpl1/FuzzyImpl.py
+++ FuzzyImpl1/FuzzyImpl.py
@@ -1,35 +1,36 @@
 
 import numpy as np
 
-def main():
 
+def main():
     print(" Welcome! \n  The Fuzzy Program\n")
-    while(1):
+    while (1):
         print(" Choose one option bellow:")
-        value = input("1- Calc one Relevance:\n"
-                      "2- Calc interval Of Relance:")
-        if(value == str(1)):
+        print("1- Calc one Relevance:")
+        print("2- Calc interval Of Relance:")
+        print("3- Print interval of suport by group:")
+        value = input()
+        if (value == str(1)):
             value = input('The higher:')
             calcRelevance(value)
-        if(value == str(2)):
+        if (value == str(2)):
             calcRelavanceOptionTwo()
-            
+        if (value == str(3)):
+            printInterval()
 
 
-def calOfRelevenceTriagle(a , m ,b , x):
-
-    if(x<=a):
+def calOfRelevenceTriagle(a, m, b, x):
+    if (x <= a):
         return 0
-    elif(a<x<=m):
-        return (x-a)/(m-a)
-    elif(m<x<=b):
-        return (b-a)/(b-m)
+    elif (a < x <= m):
+        return (x - a) / (m - a)
+    elif (m < x <= b):
+        return (b - a) / (b - m)
     else:
         return 0
 
 
 def calOfRelevenceoftrapeze(a, m, n, b, x):
-
     if (x <= a):
         return 0
     elif (a < x <= m):
@@ -41,19 +42,19 @@ def calOfRelevenceoftrapeze(a, m, n, b, x):
     else:
         return 0
 
-def  calcByGroup(group, value):
 
-    if group == "Baixo" :
-        return calOfRelevenceTriagle(1,1,1.5,value)
+def calcByGroup(group, value):
+    if group == "Baixo":
+        return calOfRelevenceTriagle(1, 1, 1.5, value)
     if group == "Medio":
-        return calOfRelevenceTriagle(1,1.5,2.0, value)
+        return calOfRelevenceTriagle(1, 1.5, 2.0, value)
     if group == "Alto":
-        return calOfRelevenceTriagle(1.65,2.0,2.0, value)
+        return calOfRelevenceTriagle(1.65, 2.0, 2.0, value)
     else:
-        print("Group not Found->"+ group)
+        print("Group not Found->" + group)
 
-def  calcRelevance(value):
 
+def calcRelevance(value):
     if isfloat(value):
         value = float(value)
         uBaixo = calOfRelevenceTriagle(1, 1, 1.5, value)
@@ -89,6 +90,137 @@ def isfloat(value):
         print("Try a valid number Like 1 or 1.0")
         return False
 
-if  __name__ =='__main__':
-    main()
 
+# Imprima na tela o intervalo  correspondente ao suporte do conjunto escolhido
+
+def printInterval():
+    group = input("Type the group: ")
+    if group == "Baixo":
+        supportBaixo()
+    if group == "Medio":
+        supportMedio()
+    if group == "Alto":
+        supportAlto()
+    else:
+        print("Group not Found->" + group)
+
+
+# Imprima na tela o intervalo correspondente ao núcleo do conjunto escolhido
+
+def printCore():
+    group = input("Type the group: ")
+    if group == "Baixo":
+        supportBaixo()
+    if group == "Medio":
+        supportMedio()
+    if group == "Alto":
+        supportAlto()
+    else:
+        print("Group not Found->" + group)
+
+
+def supportBaixo():
+    count = 1
+    suportInf = 0
+    suportSup = 0
+    trigSup = True
+    trigInf = True
+
+    while (count < 2):
+        count = count + 0.00001
+        x = calOfRelevenceTriagle(1, 1, 1.5, count)
+        if (x >= 0.999):
+            suportInf = count
+            break;
+
+    count = 1
+    while (count < 2):
+        count = count + 0.00001
+        x = calOfRelevenceTriagle(1, 1, 1.5, count)
+        if (x <= 0.001):
+            suportSup = count
+            break;
+
+    if suportInf > suportSup:
+        print("[" + str(suportSup) + " , " + str(suportInf) + "]")
+    else:
+        print("[" + str(suportInf) + " , " + str(suportSup) + "]")
+
+
+def supportMedio():
+    count = 1
+    suportInf = 0
+    suportSup = 0
+    trigSup = True
+    trigInf = True
+    while (count < 2):
+        count = count + 0.00001
+        x = calOfRelevenceTriagle(1, 1.5, 2.0, count)
+        if (x >= 0.999):
+            suportInf = count
+            break;
+
+    count = 1
+    while (count < 2):
+        count = count + 0.00001
+        x = calOfRelevenceTriagle(1, 1.5, 2.0, count)
+        if (x >= 0.001):
+            suportSup = count
+            break;
+
+    if suportInf > suportSup:
+        print("[" + str(suportSup) + " , " + str(suportInf) + "]")
+    else:
+        print("[" + str(suportInf) + " , " + str(suportSup) + "]")
+
+
+def supportAlto():
+    count = 1
+    suportInf = 0
+    suportSup = 0
+    trigSup = True
+    trigInf = True
+    while (count < 2):
+        count = count + 0.00001
+        x = calOfRelevenceTriagle(1.65, 2.0, 2.0, count)
+        if (x >= 0.999):
+            suportInf = count
+            break;
+
+    count = 1
+    while (count < 2):
+        count = count + 0.00001
+        x = calOfRelevenceTriagle(1.65, 2.0, 2.0, count)
+        if (x >= 0.001):
+            suportSup = count
+            break;
+
+    if suportInf > suportSup:
+        print("[" + str(suportSup) + " , " + str(suportInf) + "]")
+    else:
+        print("[" + str(suportInf) + " , " + str(suportSup) + "]")
+
+
+def coreBaixo():
+    count = 1
+    suportInf = 0
+    suportSup = 0
+    trigSup = True
+    trigInf = True
+    while (count < 2):
+        count = count + 0.00001
+        x = calOfRelevenceTriagle(1, 1.5, 2.0, count)
+        if (x >= 0.999):
+            suportInf = count
+            while (count < 2):
+                x = calOfRelevenceTriagle(1, 1.5, 2.0, count)
+                if (x < 0.999):
+                    trigSup = count
+                    break;
+            break;
+
+        print("[" + str(suportSup) + " , " + str(suportInf) + "]")
+
+
+if __name__ == '__main__':
+    main()
